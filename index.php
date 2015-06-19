8c2dc4c811e08f2a6d274920582f26b805453cc0
Hardy <540115739@qq.com>
2015-06-19 17:07:50 +0800

    INIT (Copy from GC).

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+if(version_compare(PHP_VERSION, '5.3.0','<'))die('require PHP > 5.3.0 !');
+
+define('SITE_PATH','/buy_business/');
+define('APP_PATH','./Application/');
+define('APP_DEBUG',true);
+define('BANNER_WIDTH',1100);
+define('BANNER_HEIGHT',240);
+define('BANNER_HEIGHT_MAIN',330);
+require './ThinkPHP/ThinkPHP.php';
+?>

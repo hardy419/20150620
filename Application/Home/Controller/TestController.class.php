3ce595b78cfdf6577dba5b2bf030559266dc2f42
Hardy <540115739@qq.com>
2015-07-01 14:58:21 +0800

    Home/Index/index.html & Public/zh/

---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class TestController extends BaseController {
+    public function index(){
+        $this->display();
+    }
+    public function hardy(){
+        $this->display();
+    }
+}

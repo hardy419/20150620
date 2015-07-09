165bb063d512b297614a0ca136b09a064a4751e4
Hardy <540115739@qq.com>
2015-07-09 16:31:32 +0800

    project_detail.html, projects.html; Separate footer.html, header.html.

---
+++ Application/Home/Controller/ProjectController.class.php
@@ -0,0 +1,50 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ProjectController extends BaseController {
+
+    public function index(){
+        // For search box
+        $dblist = M('category')->select();
+        $catelist = array();
+        foreach ($dblist as $item) {
+            $catelist[$item['id']] = $item['name'];
+        }
+        $this->assign('catelist', $catelist);
+        $this->assign('categories', $dblist);
+        // language
+        $lang = I('get.lang', 'zh');
+        $this->assign ('lang', $lang);
+        $this->display('projects');
+    }
+
+    public function project_detail(){
+        $id = I('request.id', null);
+        if (empty($id) || null == $id) {
+            $this->error('');
+        }
+        $project = M('project')->where(array('id'=>$id))->select();
+        $this->assign ('project', $project[0]);
+        $certificates = explode (',', $project[0]['c_certificate']);
+        $this->assign ('certificates', $certificates);
+
+        $dblist = M('category')->select();
+        $catelist = array();
+        foreach ($dblist as $item) {
+            $catelist[$item['id']] = $item['name'];
+        }
+        $this->assign('catelist', $catelist);
+        // For hot recomm projects
+        $hot = M('project')->where(array('hot_recomm'=>'on', 'visible'=>'on'))->select();
+        $this->assign('hot', $hot);
+        // For ads
+        $ads_1 = M('ads')->where(array('status'=>1,'type'=>1))->select();
+        $ads_2 = M('ads')->where(array('status'=>1,'type'=>2))->select();
+        $this->assign('ads_1', $ads_1);
+        $this->assign('ads_2', $ads_2);
+        // language
+        $lang = I('get.lang', 'zh');
+        $this->assign ('lang', $lang);
+        $this->display();
+    }
+}

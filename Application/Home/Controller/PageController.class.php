fc0ee41caafb00438eb7d5a3c10aaba7c97b7c5e
Hardy <540115739@qq.com>
2015-07-17 18:02:28 +0800

    所有后臺可以定制的pages; footer.html, index.html, query.html, project_detail.html: BUGS fixed ( + &lang=zh/cn/en). (Left to do: contact page)

--- Application/Home/Controller/PageController.class.php
+++ Application/Home/Controller/PageController.class.php
@@ -102,4 +102,146 @@ class PageController extends BaseController {
         $this->display();
     }
 
+//////////////////////////////////////////////////////////////////////////////
+// Sellers pages
+//////////////////////////////////////////////////////////////////////////////
+
+    public function sellers(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'賣家出讓程序'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function evaluate(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'業務估值'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function sellers_why_us(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'為何選擇我們(賣家)'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function sellers_query(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'賣家查詢'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+//////////////////////////////////////////////////////////////////////////////
+// Notice Us pages
+//////////////////////////////////////////////////////////////////////////////
+
+    public function noticeUs1(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'創富做老闆'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function noticeUs2(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'創業模式之比較'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function noticeUs3(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'何謂「業務轉讓」'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function noticeUs4(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'為何選擇業務轉讓'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function noticeUs5(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'常見問題'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+//////////////////////////////////////////////////////////////////////////////
+// Last 4 pages
+//////////////////////////////////////////////////////////////////////////////
+
+    public function noticeSale1(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'開業資料庫'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function noticeSale2(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'營商資料庫'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function link(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'相關連結'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
+    public function contact(){
+        $this->common();
+        // Retrieve page contents
+        $page = M('page')->where(array('title'=>'聯絡我們'))->select();
+        $this->assign('banner', $page[0]['banner']);
+        $this->assign('content', $page[0]['content']);
+
+        $this->display();
+    }
+
 }
